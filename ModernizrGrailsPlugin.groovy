37bebc76d3ccdec1bf3011990b96f5e75a87fceb
Konstantinos Kostarellis <kosta.grails@gmail.com>
2012-06-14 20:16:14 +0200

    New Plugin-Release 2.5.3 (using grails 2.0.4)
    -using modernizr.js (2.5.3)
    -using grails 2.0.4 (changed config files accordingly)
    -using new release plugin 2.0.3
    	- added its dependency rest-client-builder:1.0.2
    -explicitly setting project to utf-8 (sts settings)
    -adjusted .gitignore

--- ModernizrGrailsPlugin.groovy
+++ ModernizrGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class ModernizrGrailsPlugin {
     // the plugin version
-    def version = "2.0.6"
+    def version = "2.5.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -30,7 +30,8 @@ grails.project.dependency.resolution = {
 
     plugins {
         build(":tomcat:$grailsVersion",
-              ":release:1.0.0") {
+              ":release:2.0.3",
+              ":rest-client-builder:1.0.2") {
             export = false
         }
     }
