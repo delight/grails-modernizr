2070c7f4c5fa98cb4650804d11aa5f50e3468ec0
Konstantinos Kostarellis <kosta.grails@gmail.com>
2013-12-22 18:38:54 +0100

    removes unnecessary Mixin from Spec

--- test/unit/modernizr/ModernirzTagLibSpec.groovy
+++ test/unit/modernizr/ModernirzTagLibSpec.groovy
@@ -2,13 +2,11 @@ package modernizr
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
-import grails.test.mixin.web.GroovyPageUnitTestMixin
 
 /**
  * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
  */
 @TestFor(ModernirzTagLib)
-@TestMixin(GroovyPageUnitTestMixin)
 class ModernirzTagLibSpec extends Specification {
 
 	def modernizrVersion = ModernizrConfig.SHIPPED_VERSION
